ed11ec6b6ae18e90a00dae3b8c685b75cb0d9cfa
zk-oak <zkacsmar5055@oakton.edu>
2024-11-04 17:03:34 -0600

    Update Group Projett.cpp
    It actually has some info now

--- Group Projett.cpp
+++ Group Projett.cpp
@@ -1,15 +1,30 @@
 //============================================================================
-// Name        : Group.cpp
-// Author      : Kazz
-// Version     :
+// Name        : Group Projett.cpp
+// Author      : Kazz/Hamza/Maribelle//Prem
+// Version     : 24/11/4
 // Copyright   : >:o
-// Description : Hello World in C++, Ansi-style
+// Description : Fighty Fight Game, Group Project
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello! Welcome to Fighty Fight Game! Please choose your Fighter by entering 1, 2, or 3" << endl; // prints !!!Hello World!!!
+	
+	cout << "Fighter 1 " << "Insert stats here" << endl;
+	cout << "Fighter 2 " << "Insert stats here" << endl;
+	cout << "Fighter 3 " << "Insert stats here" << endl;
+	// Needs stat randomizer
+
+	cout << endl; 
+
+	cout << "Game starts here"  << endl;
+	//Needs turn system
+	//Needs to know the stats of the selected fighter
+	//Needs enemy fighter
+
+	cout << "Game end message" << endl;
+
 	return 0;
 }
