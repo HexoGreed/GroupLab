c0b7ba2b8e12afa7c89f328a9dbe3b2ff17669a3
zk-oak <zkacsmar5055@oakton.edu>
2024-11-05 20:50:59 -0600

    Update Group Projett.cpp
    Added the game loop & tournament & some formatting

--- Group Projett.cpp
+++ Group Projett.cpp
@@ -1,30 +1,61 @@
 //============================================================================
-// Name        : Group Projett.cpp
+// Name        : Group.cpp
 // Author      : Kazz/Hamza/Maribelle//Prem
-// Version     : 24/11/4
+// Version     : 24/11/5
 // Copyright   : >:o
 // Description : Fighty Fight Game, Group Project
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
+#include "Kazz.cpp"
 using namespace std;
 
 int main() {
 	cout << "Hello! Welcome to Fighty Fight Game! Please choose your Fighter by entering 1, 2, or 3" << endl; // prints !!!Hello World!!!
 	
-	cout << "Fighter 1 " << "Insert stats here" << endl;
-	cout << "Fighter 2 " << "Insert stats here" << endl;
-	cout << "Fighter 3 " << "Insert stats here" << endl;
+	
+	cout << "╔════════════════════════════════════╗" << endl;
+	cout << "║" << setw(13) << "Fighter 1 " << setw(20) << "Insert stats here" << setw(6) << "║" << endl; //"fighter # " = 10 chars
+	cout << "║" << setw(13) << "Fighter 2 " << setw(20) << "Insert stats here" << setw(6) << "║" <<endl;
+	cout << "║" << setw(13) << "Fighter 3 " << setw(20) << "Insert stats here" << setw(6) << "║" << endl;
+	cout << "╚════════════════════════════════════╝" << endl;
 	// Needs stat randomizer
 
 	cout << endl; 
 
-	cout << "Game starts here"  << endl;
+	cout << setw(31) << "║ Game Start! ║"  << endl; //setw minimum 17
+	cout << setw(49) << "┌──────────┐" << endl;
+	cout << setw(29) << "│ Round 1! │" << endl;
+	cout << setw(49) << "└──────────┘" << endl;
+
+	for (int compWins = 0, playerWins = 0, i = 1; compWins <2 && playerWins <2; i++) {		
+		if (game()){
+			cout << "You lost all your health! You lost!" << endl;
+			compWins++;
+		}
+		else {
+			cout << "They lost all their health! You win!" << endl;
+			playerWins++;
+		}
+		if (compWins == 2){
+			cout << "[OTHER FIGHTER]" << " Won the tournament!" << endl;
+		}
+		else if(playerWins == 2){
+			cout << "You won the tournament!" << endl;
+		}
+		else {
+			cout << endl;
+			cout << setw(49) << "┌──────────┐" << endl;
+			cout << setw(23) << "│ Round " << i+1 << "! │" << endl;
+			cout << setw(49) << "└──────────┘" << endl;
+		}
+	}
+	
 	//Needs turn system
 	//Needs to know the stats of the selected fighter
 	//Needs enemy fighter
 
-	cout << "Game end message" << endl;
-
+	cout << "Thanks for playing our game!" << endl;
 	return 0;
 }
