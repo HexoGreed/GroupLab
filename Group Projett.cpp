abf90f88ec798e94ebdd73dcc057d0251b5b1ab3
zk-oak <zkacsmar5055@oakton.edu>
2024-11-10 16:03:04 -0600

    Update Group Projett.cpp
    Uses NewFighter.h

--- Group Projett.cpp
+++ Group Projett.cpp
@@ -1,69 +1,65 @@
 //============================================================================
 // Name        : Group.cpp
-// Author      : Kazz/Hamza/Maribelle//Prem
-// Version     : 24/11/5
+// Author      : Kazz/Hamza/Maribelle/Prem
+// Version     : 24/11/10
 // Copyright   : >:o
 // Description : Fighty Fight Game, Group Project
 //============================================================================
 
 #include <iostream>
 #include <iomanip>
+#include <random>
+
 #include "GameLoop.h"
-#include "Fighter.h"
+#include "NewFighter.h"
 using namespace std;
 
-int main() {
-	//Initializes both pickable fighter and the player
-	Fighter PickableFighters;
-	PLayer PlayerFighter;
-	//
+// TEMP CODE
+int fighterChoice;
+Fighters PickableFighters;
 
-		
+int main() {
 	cout << "Hello! Welcome to Fighty Fight Game! Please choose your Fighter by entering 1, 2, or 3" << endl; // prints !!!Hello World!!!
 	
-	
+	/*
 	cout << "╔════════════════════════════════════╗" << endl;
-	cout << "║\n"; PickableFighters.checkFighterStats();
-	cout << "║";
+	cout << "║" << setw(13) << "Fighter 1 " << setw(20) << "Insert stats here" << setw(6) << "║" << endl; //"fighter # " = 10 chars
+	cout << "║" << setw(13) << "Fighter 2 " << setw(20) << "Insert stats here" << setw(6) << "║" <<endl;
+	cout << "║" << setw(13) << "Fighter 3 " << setw(20) << "Insert stats here" << setw(6) << "║" << endl;
 	cout << "╚════════════════════════════════════╝" << endl;
-	// its shows up weird dont worry about it;
+	*/
+	PickableFighters.checkFighterStats();
+	cout << endl;
 
-	cout << endl; 
-
-	//this is for the reroll thing;
 	cout << "do you wish to reroll? Y OR N\n";
 	char choice;
 	cin >> choice;
-	if (choice == 'Y') {
-		PickableFighters.ReRoll(0);
-		PickableFighters.ReRoll(1);
-		PickableFighters.ReRoll(2);
+	if (choice == 'Y') { //fix formatting when hitting yes, and also make yes not case sensitive.
+		PickableFighters.ReRollAll();
 
+		PickableFighters.checkFighterStats();
+		cout << endl;
 	}
-
-	//THIS is all for making the player fighter =  the picked fighter;
-	std::pair<int, int> chosenFighter = PickableFighters.pickFighter();
-	PlayerFighter.setPlayerStats(chosenFighter);
-	PlayerFighter.displayPlayerStats();
-	//cout << "\n\n\n";
 	
+	Fighter& fighter = PickableFighters.pickFighter();
 
 
-	//end of reroll and start of battle, we can add a while loop if we want them t be able to reroll
+
+
+	cout << endl; 
+
 	cout << setw(31) << "║ Game Start! ║"  << endl; //setw minimum 17
 	cout << setw(49) << "┌──────────┐" << endl;
 	cout << setw(29) << "│ Round 1! │" << endl;
 	cout << setw(49) << "└──────────┘" << endl;
 
-
-
-
 	for (int compWins = 0, playerWins = 0, i = 1; compWins <2 && playerWins <2; i++) {		
-		if (game()){
+		if (game(fighter.HP(), fighter.Attack())){
 			cout << "You lost all your health! You lost!" << endl;
 			compWins++;
 		}
 		else {
+		
 			cout << "They lost all their health! You win!" << endl;
 			playerWins++;
 		}
@@ -80,9 +76,7 @@ int main() {
 			cout << setw(49) << "└──────────┘" << endl;
 		}
 	}
-	
-	//Needs turn system
-	//Needs to know the stats of the selected fighter
+
 	//Needs enemy fighter
 
 	cout << "Thanks for playing our game!" << endl;
