81f478e27977211dd0ceb09abe63806f6f4adcee
zk-oak <zkacsmar5055@oakton.edu>
2024-11-11 15:59:20 -0600

    Update HatClass.cpp
    Added functionality so it could work with the rest of the code

--- HatClass.cpp
+++ HatClass.cpp
@@ -9,24 +9,53 @@ private:
     string HatChoice;
 
 public:
+    Hat();
+    ~Hat();
+    int hatCode();
+    void chooseHat();
+    void startHat();
+    void endHat();
+};
 
-    void chooseHat() {
-        cout << "Enter the name of the hat you want to wear (Cowboy Hat, Party Hat, Chef Hat): ";
-        getline(cin, HatChoice);
+Hat::Hat(){
+}
 
-        if (HatChoice == "Cowboy Hat" or HatChoice == "Party Hat" or HatChoice == "Chef Hat") {
+Hat::~Hat(){
+}
+
+void Hat::chooseHat() {
+    cout << "Enter the name of the hat you want to wear (Cowboy Hat, Party Hat, Chef Hat): ";
+    
+    cin.clear();
+    cin.ignore(256, '\n');
+    while (1){
+        getline(cin, HatChoice); 
+        if (HatChoice == "cowboy hat" or HatChoice == "party hat" or HatChoice == "chef hat") {
             cout << "You are wearing a " << HatChoice << "!" << endl;
+            return;
         } else {
             cout << "Invalid choice! Please enter either Cowboy Hat, Party Hat, or Chef Hat." << endl;
         }
     }
-};
+}
 
-/*
-int main() {
-    Hat hat; //put this in main at the end
-    hat.chooseHat(); //put this in main at the end
+void Hat::startHat() {
+    cout << "\033[" << hatCode() << "m";
+}
 
+void Hat::endHat() {
+    cout << "\033[0m";
+}
+
+int Hat::hatCode() {
+    if(HatChoice == "cowboy hat"){
+        return 33;
+    }
+    else if(HatChoice == "party hat"){
+        return 35;
+    }
+    else if (HatChoice == "chef hat"){
+        return 36;
+    }
     return 0;
 }
-*/
